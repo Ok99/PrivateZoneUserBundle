d1af7150bc49d0efac29732c19170814949ce4b4
Tomáš Vítek <vitekt@email.cz>
2026-04-08 23:24:03 +0200

    Fix of user load when its a host

--- Security/UserProvider.php
+++ Security/UserProvider.php
@@ -2,11 +2,21 @@
 
 namespace Ok99\PrivateZoneCore\UserBundle\Security;
 
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+
 class UserProvider extends \FOS\UserBundle\Security\UserProvider
 {
 
     public function loadUserByUsername($username)
     {
+        try {
+            return parent::loadUserByUsername($username);
+        } catch (UsernameNotFoundException $e) {
+            if (strlen($username) !== 7) {
+                throw $e;
+            }
+        }
+
         if (strlen($username) === 7) {
             $username = substr($username, 3);
         }
